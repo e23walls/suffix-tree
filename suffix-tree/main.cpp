479fd6e08808c16ef52f7b38a69daa173a2f9456
Emily Walls <emilywalls23@gmail.com>
2016-03-04 21:23:56 -0500

    Initial Commit

---
+++ suffix-tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  suffix-tree
+//
+//  Created by Emily Walls on 2016-03-04.
+//  Copyright © 2016 Emily Walls. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
